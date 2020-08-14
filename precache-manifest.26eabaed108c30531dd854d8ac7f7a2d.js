081ce47a8719304e5dec658baa2de213de13edfb
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-08-14 23:15:50 +0300

    Updates

---
+++ precache-manifest.26eabaed108c30531dd854d8ac7f7a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da4ee0e3126ce6b711d085eed6573dc",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "69d7a4ea767b941119ad",
+    "url": "/Tic-tac-toe/static/css/main.0c0a7eb2.chunk.css"
+  },
+  {
+    "revision": "435c80cc852287c361e2",
+    "url": "/Tic-tac-toe/static/js/2.930e8bde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-tac-toe/static/js/2.930e8bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d7a4ea767b941119ad",
+    "url": "/Tic-tac-toe/static/js/main.4205f004.chunk.js"
+  },
+  {
+    "revision": "096f51d48281d1a1a360",
+    "url": "/Tic-tac-toe/static/js/runtime-main.4849eb50.js"
+  }
+]);
